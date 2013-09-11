f7bc6d41d49003dfcae6c44044e9267a0ed9cfb2
unknown <DRogozhin@mrk-i0318.amust.local>
2013-09-11 20:06:34 +0400

    wip2

--- querybuilder.js
+++ querybuilder.js
@@ -1,34 +1,62 @@
 "use strict"; /*jslint node: true */
 
 var Builder = {},
-    querystring = require("querystring"),
+    querystring = require( "querystring" ),
     parse = querystring.parse;
 
+function construct( url, parameters, fragment, sep, eq ){
+    return url.concat( "?", querystring.stringify( parameters, sep, eq ), fragment );
+};
+
+function parseURL( url ){
+    var safeUrl = "" + url,
+        queryStart = safeUrl.indexOf( "?" ),
+        fragmentStart = safeUrl.indexOf( "#" ),
+        fragment = ~fragmentStart ? safeUrl.substring( fragmentStart ) : "",
+        urlPart = safeUrl.substring( 0, ~queryStart ? queryStart : safeUrl.length - fragment.length ),
+        oldQuery = ~queryStart
+        ? ( safeUrl.substring( 1 + queryStart, ~fragmentStart ? fragmentStart : undefined ) ) : "";
+    return [ urlPart, oldQuery, fragment ];
+};
+
+function buildParameters( parameters, oldQuery, mode ){
+    var oldParameters,
+        newParameters = typeof parameters === "string"
+        ? parse( parameters )
+        : new Object( parameters );
+
+    if( typeof mode !== "undefined" && mode.merge ){
+        oldParameters = parse( oldQuery );
+        // Merge new parameters into old query
+        Object.getOwnPropertyNames( parameters ).forEach( function( key ){
+            var value = parameters[ key ];
+            if ( oldParameters[ key ] !== value )
+                oldParameters[ key ] = value;
+        });
+        return oldParameters;
+    } else {
+        return newParameters;
+    }
+};
+
 // Function produces new query URL from `url` string and `parameters`
-// object. It leaves old query params and replace matched with new ones
+// object. It leaves old query parameters and replace matched with new ones
 //
 //  url - string with arbitraty URL
 //  parameters is required, parameters for query separator and
 //  equivalency are optional
-exports.merge = Builder.merge = function( url, parameters, separator, equivalency ) {
-  var queryStart = url.indexOf( "?" ),
-      fragmentStart = url.indexOf( "#" ),
-      fragment = ~fragmentStart ? url.substring( fragmentStart ) : "";
-      urlPart = url.substring( 0, ~queryStart ? queryStart : url.length - fragment.length ),
-      oldQuery = ~queryStart
-        ? ( url.substring( 1 + queryStart, ~fragmentStart ? fragmentStart - 1 : undefined ) )
-        : "",
-      oldParameters = parse( oldQuery );
-  
-  parameters = typeof parameters === "string"
-    ? parse( parameters )
-    : parameters;
 
+exports.merge = Builder.merge = function( url, parameters, separator, equivalency ) {
+    var urlParts = parseURL( url ),
+        newParameters = buildParameters( parameters, urlParts[ 1 ], { merge: 1 });
+    return construct( urlParts[ 0 ], newParameters, urlParts[ 2 ], separator, equivalency );
 };
 
 // Function produces new query URL from `url` string and `parameters`
 // object. It completely replace old query with new one
 
-exports.replace = Builder.merge = function( url, parameters, separator, equivalency ) {
-  
+exports.replace = Builder.replace = function( url, parameters, separator, equivalency ) {
+    var urlParts = parseURL( url ),
+        newParameters = buildParameters( parameters );
+    return construct( urlParts[ 0 ], newParameters, urlParts[ 2 ], separator, equivalency );
 };
--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+var assert = require( "assert" );
 
+describe( "Query Builder", function(){
+    var simple = {
+        url: "http://test.local?q=parsed",
+        param: { test: "passed", another: "passed" },
+        replaceResult: "http://test.local?test=passed&another=passed"
+    }
+    describe( "module API", function(){
+
+        it( "should exports 'replace' function", function(){
+            replace = require( "./querybuilder" ).replace;
+            assert.equal( typeof replace, "function" );
+        });
+
+        it( "Simple replace gives " + simple.replaceResult, function(){
+            assert.equal( replace( simple.url, simple.param), simple.replaceResult );
+        });
+
+    });
+});
